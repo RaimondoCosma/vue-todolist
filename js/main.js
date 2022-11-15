e1021d435fff4c66ee321a4c6f7edadeea88f1af
RaimondoCosma <raimondo.cosma@gmail.com>
2022-11-15 15:03:07 +0100

    Aggiunge bozza di lista tramite vue

--- js/main.js
+++ js/main.js
@@ -6,7 +6,20 @@ const { createApp } = Vue
 createApp({
   data() {
     return {
-      message: 'Hello Vue!'
+      myList: [
+        {
+            text: "Fare la spesa",
+            done: false,
+        },
+        {
+            text: "Vedere un film",
+            done: false,
+        },
+        {
+            text: "Completare l'esercizio di oggi",
+            done: false,
+        },
+      ]
     }
   }
 }).mount('#app')
